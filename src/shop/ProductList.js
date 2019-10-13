13db08f87495897d80ec6cc30668045c34c16b21
Leo Wildt <leonardod.trimarchi@gmail.com>
2019-10-13 17:22:02 -0400

    chapter 6 changes

--- src/shop/ProductList.js
+++ src/shop/ProductList.js
@@ -18,7 +18,7 @@ export class ProductList extends Component {
                         { p.description }
                         <button className="btn btn-success btn-sm float-right"
                             onClick={ () => this.props.addToCart(p) } >
-                                Add To Cart Test
+                                Add To Cart
                         </button>                                            
                     </div>                
                 </div>
